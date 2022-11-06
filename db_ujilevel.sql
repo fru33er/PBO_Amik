d3222f4edce3dedfd2ec4eefef49b1a22c3a840a
fru33er <64488669+fru33er@users.noreply.github.com>
2022-11-06 22:12:20 +0700

    Add files via upload

---
+++ db_ujilevel.sql
@@ -0,0 +1,285 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3307
+-- Generation Time: Nov 06, 2022 at 04:11 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_ujilevel`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `total_harga_transaksi` ()   BEGIN
+SELECT 
+SUM(tb_keranjang.jumlah*tb_keranjang.harga) AS total_harga
+FROM tb_keranjang;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'mr_admin', '12345'),
+(2, 'admin', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_databarang`
+--
+
+CREATE TABLE `tb_databarang` (
+  `kode_barang` int(50) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `stok` int(10) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_databarang`
+--
+
+INSERT INTO `tb_databarang` (`kode_barang`, `nama_barang`, `harga`, `stok`, `tanggal`) VALUES
+(2000, 'Kopi Hitam', 5000, 279, '2020-03-26'),
+(2001, 'Kopi Susu', 5000, 960, '2020-03-09'),
+(2004, 'Teh Tarik', 5000, 389, '2020-03-09'),
+(2005, 'Lemon Tea', 5000, 480, '2020-03-09'),
+(2006, 'Milkshake', 10000, 202, '2020-03-09'),
+(2007, 'Cappuchino', 7500, 485, '2020-03-13'),
+(2013, 'Caramel Macchiato', 7500, 90, '2020-04-02'),
+(2015, 'Roti Bakar', 15000, 91, '2020-04-03'),
+(2020, 'Croissant', 8000, 1397, '2020-04-04'),
+(2026, 'Mie Ayam', 10000, 92, '2022-10-31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_datapetugas`
+--
+
+CREATE TABLE `tb_datapetugas` (
+  `id_petugas` int(11) NOT NULL,
+  `nama_petugas` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_datapetugas`
+--
+
+INSERT INTO `tb_datapetugas` (`id_petugas`, `nama_petugas`, `email`, `alamat`, `username`, `password`) VALUES
+(13, 'raka', 'raka4100@gmail.com', 'Bogor', 'raka', 'mftnrk8b'),
+(16, 'deden', 'deden4100@gmail.com', 'sentul', 'deden', 'deden123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_keranjang`
+--
+
+CREATE TABLE `tb_keranjang` (
+  `id_transaksi` int(11) NOT NULL,
+  `kode_barang` int(10) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `total_harga` int(10) NOT NULL,
+  `tgl_transaksi` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Triggers `tb_keranjang`
+--
+DELIMITER $$
+CREATE TRIGGER `cancel` AFTER DELETE ON `tb_keranjang` FOR EACH ROW BEGIN
+UPDATE tb_databarang SET
+stok = stok + OLD.jumlah
+WHERE kode_barang = OLD.kode_barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `cancel_2` AFTER DELETE ON `tb_keranjang` FOR EACH ROW BEGIN
+DELETE FROM transaksi
+WHERE kode_barang = OLD.kode_barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `stok_habis` AFTER INSERT ON `tb_keranjang` FOR EACH ROW BEGIN
+DELETE FROM tb_databarang
+WHERE stok = 0
+AND
+kode_barang = NEW.kode_barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `tgl_transaksi` date NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `kode_barang` int(50) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jumlah_barang` int(10) NOT NULL,
+  `total_harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`tgl_transaksi`, `id_transaksi`, `kode_barang`, `nama_barang`, `harga`, `jumlah_barang`, `total_harga`) VALUES
+('2022-10-31', 1, 2015, 'Roti Bakar', 15000, 1, 15000),
+('2022-10-31', 2, 2020, 'Croissant', 8000, 3, 24000),
+('2022-10-31', 3, 2004, 'Teh Tarik', 5000, 2, 10000),
+('2022-10-31', 4, 2000, 'Kopi Hitam', 5000, 1, 5000),
+('2022-10-31', 5, 2015, 'Roti Bakar', 15000, 3, 45000),
+('2022-10-31', 6, 2004, 'Teh Tarik', 5000, 5, 25000),
+('2022-10-31', 7, 2000, 'Kopi Hitam', 5000, 1, 5000),
+('2022-10-31', 8, 2026, 'Mie Ayam', 10000, 3, 30000),
+('2022-10-31', 9, 2000, 'Kopi Hitam', 5000, 5, 25000),
+('2022-10-31', 10, 2026, 'Mie Ayam', 10000, 5, 50000),
+('2022-10-31', 11, 2006, 'Milkshake', 10000, 3, 30000),
+('2022-10-31', 12, 2000, 'Kopi Hitam', 5000, 1, 5000);
+
+--
+-- Triggers `transaksi`
+--
+DELIMITER $$
+CREATE TRIGGER `keranjang` AFTER INSERT ON `transaksi` FOR EACH ROW BEGIN
+INSERT INTO tb_keranjang SET
+id_transaksi = NEW.id_transaksi,
+kode_barang = NEW.kode_barang,
+nama_barang = NEW.nama_barang,
+harga = NEW.harga,
+jumlah = NEW.jumlah_barang,
+total_harga = NEW.total_harga,
+tgl_transaksi = NEW.tgl_transaksi;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `transaksi` AFTER INSERT ON `transaksi` FOR EACH ROW BEGIN
+UPDATE tb_databarang SET
+stok = stok - NEW.jumlah_barang
+WHERE kode_barang = NEW.kode_barang;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_databarang`
+--
+ALTER TABLE `tb_databarang`
+  ADD PRIMARY KEY (`kode_barang`);
+
+--
+-- Indexes for table `tb_datapetugas`
+--
+ALTER TABLE `tb_datapetugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- Indexes for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `kode_barang` (`kode_barang`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `kode_barang` (`kode_barang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_databarang`
+--
+ALTER TABLE `tb_databarang`
+  MODIFY `kode_barang` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2027;
+
+--
+-- AUTO_INCREMENT for table `tb_datapetugas`
+--
+ALTER TABLE `tb_datapetugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
